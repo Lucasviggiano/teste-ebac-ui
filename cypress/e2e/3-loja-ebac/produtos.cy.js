2d4d6becc948274c754cc6153b85fb937a7ba51a
Lucas VE <viggiano.luca@gmail.com>
2024-06-10 18:21:46 -0300

    teste de produtos.

---
+++ cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -0,0 +1,20 @@
+/// <reference types="cypress" />
+
+describe('funcionalidade: produtos', () => {
+
+    beforeEach(() => {
+        cy.visit('http://lojaebac.ebaconline.art.br/produtos/')
+    });
+
+
+    it('deve selecionar um produto da lista', () => {
+        cy.get('.product-block > .block-inner >')
+            //.first()
+
+            .eq(3)
+            .click()
+
+        cy.get('#tab-title-description > a').should('contain','Descrição')
+
+    });
+});
