48d8c7f42d273e8a5714b1b75e292a7152501154
Lucas VE <viggiano.luca@gmail.com>
2024-06-13 16:46:12 -0300

    cypress config

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,16 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: "8uaa2z",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
 baseUrl: 'http://lojaebac.ebaconline.art.br/'
+
+
+
+
 
   },
 });
